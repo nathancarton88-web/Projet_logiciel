7329e4f72897acd254966744974b03268159bf78
Nathan <nathan.carton88@gmail.com>
2026-05-11 14:50:04 +0200

    ajout de l'autre projet map personnage pour fusion

---
+++ map.py
@@ -0,0 +1,79 @@
+import pygame
+import pyscroll
+import pytmx
+
+from player import Player
+from screen import Screen
+from change import Change
+
+
+class Map:
+    def __init__(self, screen: Screen):
+        self.screen: Screen = screen
+        self.tmx_data: pytmx.TiledMap | None = None
+        self.map_layer: pyscroll.BufferedRenderer | None = None
+        self.group: pyscroll.PyscrollGroup | None = None
+
+        self.player: Player | None = None
+        self.change: list[Change] | None = None
+        self.collisions: list[pygame.Rect] | None = None
+
+        self.current_map: Change = Change("switch", "map_0", pygame.Rect(0, 0, 0, 0), 0)
+
+        self.switch_map(self.current_map)
+
+    def switch_map(self, change: Change) -> None:
+        self.tmx_data = pytmx.load_pygame(f"C:/Users/natha/PycharmProjects/PokePoke/assets/map/{change.name}.tmx")
+        map_data = pyscroll.data.TiledMapData(self.tmx_data)
+        self.map_layer = pyscroll.BufferedRenderer(map_data, self.screen.get_size())
+        self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=7)
+
+        if change.name.split("_")[0] == "map":
+            self.map_layer.zoom = 3
+        else:
+            self.map_layer.zoom = 3.75
+
+        self.change = []
+        self.collisions = []
+
+        for obj in self.tmx_data.objects:
+            if obj.name == "collision":
+                self.collisions.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+            type = obj.name.split(" ")[0]
+            if type == "switch":
+                self.change.append(Change(
+                    type, obj.name.split(" ")[1], pygame.Rect(obj.x, obj.y, obj.width, obj.height),
+                    int(obj.name.split(" ")[-1])
+                ))
+
+        if self.player:
+            self.pose_player(change)
+            self.player.align_hitbox()
+            self.player.step = 16
+            self.player.add_switchs(self.change)
+            self.player.add_collisions(self.collisions)
+            self.group.add(self.player)
+            if change.name.split("_")[0] != "map":
+                self.player.switch_bike(True)
+
+        self.current_map = change
+
+    def add_player(self, player) -> None:
+        self.group.add(player)
+        self.player = player
+        self.player.align_hitbox()
+        self.player.add_switchs(self.change)
+        self.player.add_collisions(self.collisions)
+
+    def update(self) -> None:
+        if self.player:
+            if self.player.change_map and self.player.step >= 8:
+                self.switch_map(self.player.change_map)
+                self.player.change_map = None
+        self.group.update()
+        self.group.center(self.player.rect.center)
+        self.group.draw(self.screen.get_display())
+
+    def pose_player(self, change: Change):
+        position = self.tmx_data.get_object_by_name("spawn " + self.current_map.name + " " + str(change.port))
+        self.player.position = pygame.math.Vector2(position.x, position.y)
---
+++ player.py
@@ -0,0 +1,88 @@
+import pygame
+
+from elcode import change
+from entity import Entity
+from keylistener import KeyListener
+from screen import Screen
+from change import Change
+
+
+class Player(Entity):
+    def __init__(self, keylistener: KeyListener, screen: Screen, x: int, y: int):
+        super().__init__(keylistener, screen, x, y)
+        self.pokedollars: int = 0
+
+        self.spritesheet_bike: pygame.image = pygame.image.load("C:/Users/natha/PycharmProjects/PokePoke/assets/sprite/hero_01_white_f_cycle_wheel.png")
+
+        self.change: list[Change] | None = None
+        self.collisions: list[pygame.Rect] | None = None
+        self.change_map: Change | None = None
+
+    def update(self) -> None:
+        self.check_input()
+        self.check_move()
+        super().update()
+
+    def check_move(self) -> None:
+        if self.animation_walk is False:
+            temp_hitbox = self.hitbox.copy()
+            if self.keylistener.key_pressed(pygame.K_LEFT):
+                temp_hitbox.x -= 16
+                if not self.check_collisions(temp_hitbox):
+                    self.check_collisions_switchs(temp_hitbox)
+                    self.move_left()
+                else:
+                    self.direction = "left"
+            elif self.keylistener.key_pressed(pygame.K_RIGHT):
+                temp_hitbox.x += 16
+                if not self.check_collisions(temp_hitbox):
+                    self.check_collisions_switchs(temp_hitbox)
+                    self.move_right()
+                else:
+                    self.direction = "right"
+            elif self.keylistener.key_pressed(pygame.K_UP):
+                temp_hitbox.y -= 16
+                if not self.check_collisions(temp_hitbox):
+                    self.check_collisions_switchs(temp_hitbox)
+                    self.move_up()
+                else:
+                    self.direction = "up"
+            elif self.keylistener.key_pressed(pygame.K_DOWN):
+                temp_hitbox.y += 16
+                if not self.check_collisions(temp_hitbox):
+                    self.check_collisions_switchs(temp_hitbox)
+                    self.move_down()
+                else:
+                    self.direction = "down"
+
+    def add_switchs(self, change: list[Change]):
+        self.change = change
+
+    def check_collisions_switchs(self, temp_hitbox):
+        if self.change:
+            for switch in self.change:
+                if switch.check_collision(temp_hitbox):
+                    self.change_map = switch
+        return None
+
+    def add_collisions(self, collisions):
+        self.collisions = collisions
+
+    def check_collisions(self, temp_hitbox: pygame.Rect):
+        for collision in self.collisions:
+            if temp_hitbox.colliderect(collision):
+                return True
+        return False
+
+    def check_input(self):
+        if self.keylistener.key_pressed(pygame.K_b):
+            self.switch_bike()
+
+    def switch_bike(self, deactive=False):
+        if self.speed == 1 and not deactive:
+            self.speed = 2
+            self.all_images = self.get_all_images(self.spritesheet_bike)
+        else:
+            self.speed = 1
+            self.all_images = self.get_all_images(self.spritesheet)
+        self.keylistener.remove_key(pygame.K_b)
